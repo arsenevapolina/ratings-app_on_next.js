7ed8016219e6de881b0ba2f22b1fa903707035ce
Полина <pechenkova.polina.mgu@yandex.ru>
2025-01-26 08:31:08 +0300

    feature - added Head and Document components

---
+++ top-app/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head"; 
+import { JSX } from "react";
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Ratings-app</title>
+        <link key={1} rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
