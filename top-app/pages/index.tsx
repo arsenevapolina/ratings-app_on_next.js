17d10e3f12472cc108b2e7e78dc70ec556c154d9
Полина <pechenkova.polina.mgu@yandex.ru>
2025-01-27 02:44:44 +0300

    useState()

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -2,13 +2,18 @@ import { Button } from "@/components/Button/Button";
 import Htag from "@/components/Htag/Htag";
 import P from "@/components/P/P";
 import Tag from "@/components/Tag/Tag";
-import { JSX } from "react";
+import { JSX, useState } from "react";
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <>
-      <Htag tag="h1">Текст</Htag>
-      <Button appearence="primary" arrow="right">
+      <Htag tag="h1">{counter}</Htag>
+      <Button
+        appearence="primary"
+        arrow="right"
+        onClick={() => setCounter((x) => x + 1)}
+      >
         Кнопка
       </Button>
       <Button appearence="ghost" arrow="down">
