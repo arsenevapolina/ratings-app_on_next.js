b781e314324d148f59044a0a5732260a1bb6481f
Полина <pechenkova.polina.mgu@yandex.ru>
2025-01-26 17:11:35 +0300

    feature - added button component

---
+++ top-app/components/Button/Button.props.ts
@@ -0,0 +1,11 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+
+export interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  appearence: "primary" | "ghost";
+  children: ReactNode;
+  arrow?: "right" | "down" | "none";
+}
--- top-app/pages/_app.tsx
+++ top-app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head"; 
+import Head from "next/head";
 import { JSX } from "react";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
@@ -9,6 +9,14 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>Ratings-app</title>
         <link key={1} rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;400;500;700;900&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
