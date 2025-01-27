335c75464445d19a5dd9ca5dc25ba3cede7571c0
Полина <pechenkova.polina.mgu@yandex.ru>
2025-01-27 22:32:28 +0300

    feature - implemented footer

--- top-app/layout/Footer/Footer.tsx
+++ top-app/layout/Footer/Footer.tsx
@@ -1,6 +1,19 @@
 import React, { JSX } from "react";
 import { FooterProps } from "./Footer.props";
+import cn from "classnames";
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
