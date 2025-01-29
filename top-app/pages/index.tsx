7d1656ce196799389ec4e885c532fecb00388c18
Полина <pechenkova.polina.mgu@yandex.ru>
2025-01-29 05:18:48 +0300

    feature - received category menu within SSR

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -5,8 +5,11 @@ import Rating from "@/components/Rating/Rating";
 import Tag from "@/components/Tag/Tag";
 import { withLayout } from "@/layout/Layout";
 import { JSX, useState } from "react";
+import axios from "axios";
+import { GetStaticProps } from "next/types";
+import { MenuItem } from "@/interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -29,8 +32,34 @@ function Home(): JSX.Element {
         Green
       </Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
