fee65c28ea39f8029e48f4cbeebe7915ae9dca3a
Полина <pechenkova.polina.mgu@yandex.ru>
2025-02-05 02:43:15 +0300

    feature - added Sidebar

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,10 +1,5 @@
-import { Button } from "@/components/Button/Button";
-import Htag from "@/components/Htag/Htag";
-import P from "@/components/P/P";
-import Rating from "@/components/Rating/Rating";
-import Tag from "@/components/Tag/Tag";
 import { withLayout } from "@/layout/Layout";
-import { JSX, useState } from "react";
+import { JSX } from "react";
 import axios from "axios";
 import { GetStaticProps } from "next";
 import { MenuItem } from "@/interfaces/menu.interface";
@@ -14,36 +9,8 @@ interface HomeProps extends Record<string, unknown> {
   firstCategory: number;
 }
 
-function Home({ menu }: HomeProps): JSX.Element {
-  const [rating, setRating] = useState<number>(4);
-
-  return (
-    <>
-      <Htag tag="h1">Заголовок</Htag>
-      <Button appearence="primary" arrow="right">
-        Кнопка
-      </Button>
-      <Button appearence="ghost" arrow="down">
-        Кнопка
-      </Button>
-      <P size="large">Большой</P>
-      <P>Средний</P>
-      <P size="small">Маленький</P>
-      <Tag size="small">Ghost</Tag>
-      <Tag size="medium" color="red">
-        Red
-      </Tag>
-      <Tag size="small" color="green">
-        Green
-      </Tag>
-      <Rating rating={rating} isEditable setRating={setRating} />
-      <ul>
-        {menu.map((m) => (
-          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
-        ))}
-      </ul>
-    </>
-  );
+function Home({}: HomeProps): JSX.Element {
+  return <></>;
 }
 
 export default withLayout(Home);
