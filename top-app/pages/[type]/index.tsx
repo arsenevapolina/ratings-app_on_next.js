691815432baaadcf39621ae53cf7beec255130cd
Полина <pechenkova.polina.mgu@yandex.ru>
2025-02-05 02:28:37 +0300

    feature - improved routing implementation

---
+++ top-app/pages/[type]/index.tsx
@@ -0,0 +1,62 @@
+import { withLayout } from "@/layout/Layout";
+import { JSX } from "react";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { MenuItem } from "@/interfaces/menu.interface";
+import { firstLevelMenu } from "@/helpers/helpers";
+
+interface TypeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
+
+function Type({ firstCategory }: TypeProps): JSX.Element {
+  return <>Type: {firstCategory}</>;
+}
+
+export default withLayout(Type);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: firstLevelMenu.map((m) => "/" + m.route),
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<TypeProps> = async ({ params }) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const firstCategoryItem = firstLevelMenu.find((m) => m.route === params.type);
+
+  if (!firstCategoryItem) {
+    return {
+      notFound: true,
+    };
+  }
+
+  try {
+    const { data: menu } = await axios.post<MenuItem[]>(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`,
+      { firstCategory: firstCategoryItem.id }
+    );
+
+    return {
+      props: {
+        menu,
+        firstCategory: firstCategoryItem.id,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        menu: [],
+        firstCategory: firstCategoryItem.id,
+      },
+    };
+  }
+};
---
+++ top-app/pages/search.tsx
@@ -0,0 +1,47 @@
+import { withLayout } from "@/layout/Layout";
+import { JSX } from "react";
+import axios from "axios";
+import { GetStaticProps } from "next";
+import { MenuItem } from "@/interfaces/menu.interface";
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
+
+function Search(): JSX.Element {
+
+  return (
+    <>
+      Search
+    </>
+  );
+}
+
+export default withLayout(Search);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+
+  try {
+    const { data: menu } = await axios.post<MenuItem[]>(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`,
+      { firstCategory }
+    );
+
+    return {
+      props: {
+        menu,
+        firstCategory,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        menu: [],
+        firstCategory,
+      },
+    };
+  }
+};
