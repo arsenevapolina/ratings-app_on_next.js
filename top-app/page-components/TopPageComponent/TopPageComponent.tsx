473f9780aa3902e87071934fa1d905f4ae7deac3
Полина <pechenkova.polina.mgu@yandex.ru>
2025-02-06 22:29:23 +0300

    feature - products page implemented

--- top-app/page-components/TopPageComponent/TopPageComponent.tsx
+++ top-app/page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,10 +1,37 @@
 import { JSX } from "react";
 import { TopPageComponentProps } from "./TopPageComponent.props";
+import Htag from "@/components/Htag/Htag";
+import styles from "./TopPageComponent.module.css";
+import Tag from "@/components/Tag/Tag";
+import { HhData } from "@/components/HhData/hhData";
+import { TopLevelCategory } from "@/interfaces/page.interface";
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="small">
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортировка</span>
+      </div>
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        <Tag color="red" size="small">
+          hh.ru
+        </Tag>
+      </div>
+      {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+    </div>
+  );
 };
