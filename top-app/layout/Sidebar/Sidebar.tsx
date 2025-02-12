b1c3fca4b9bd9f591d13a78515ec44319224a16e
Полина <pechenkova.polina.mgu@yandex.ru>
2025-02-13 01:09:51 +0300

    feature - Search component added

--- top-app/layout/Sidebar/Sidebar.tsx
+++ top-app/layout/Sidebar/Sidebar.tsx
@@ -4,13 +4,13 @@ import { Menu } from "../Menu/Menu";
 import Logo from "../logo.svg";
 import styles from "./Sidebar.module.css";
 import cn from "classnames";
+import { Search } from "@/components";
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
     <div className={cn(className, styles.sidebar)} {...props}>
       <Logo className={styles.logo} />
-      <div>Поиск</div>
-
+      <Search />
       <Menu />
     </div>
   );
