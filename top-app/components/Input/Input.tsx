f0380ecfa776caeeb66d92d574379d8870feeca4
Полина <pechenkova.polina.mgu@yandex.ru>
2025-02-12 20:20:32 +0300

    feature - added Input and Textarea components

---
+++ top-app/components/Input/Input.tsx
@@ -0,0 +1,8 @@
+import React, { JSX } from "react";
+import { InputProps } from "./Input.props";
+import cn from "classnames";
+import styles from "./Input.module.css";
+
+export const Input = ({ className, ...props }: InputProps): JSX.Element => {
+  return <input className={cn(className, styles.input)} {...props} />;
+};
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -3,6 +3,7 @@ import { JSX } from "react";
 import axios from "axios";
 import { GetStaticProps } from "next";
 import { MenuItem } from "@/interfaces/menu.interface";
+import { Input, Textarea } from "@/components";
 
 interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
@@ -10,7 +11,12 @@ interface HomeProps extends Record<string, unknown> {
 }
 
 function Home({}: HomeProps): JSX.Element {
-  return <></>;
+  return (
+    <>
+      <Input placeholder="тест" />
+      <Textarea placeholder="тест area" />
+    </>
+  );
 }
 
 export default withLayout(Home);
