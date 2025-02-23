117c03ac4c8e307f43f0b9674680dd7f84d0ad26
Полина <pechenkova.polina.mgu@yandex.ru>
2025-02-24 02:07:36 +0300

    feature -  added scrolling component using useAnimation hook

---
+++ top-app/components/Up/Up.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import styles from "./Up.module.css";
+import { JSX } from "react/jsx-runtime";
+import UpIcon from "./Up.svg";
+import { useScrollY } from "@/hooks/useScrollY";
+import { motion, useAnimation } from "framer-motion";
+
+export const Up = (): JSX.Element => {
+  const controls = useAnimation();
+  const y = useScrollY();
+
+  useEffect(() => {
+    controls.start({ opacity: y / document.body.scrollHeight });
+  }, [y, controls]);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <motion.button
+      className={styles.up}
+      onClick={scrollToTop}
+      animate={controls}
+      initial={{ opacity: 0 }}
+    >
+      <UpIcon />
+    </motion.button>
+  );
+};
