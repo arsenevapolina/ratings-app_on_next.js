d10d5178978c3a4de323d204aa2a6d851a97df0e
Полина <pechenkova.polina.mgu@yandex.ru>
2025-03-05 20:33:09 +0300

    feature - yandex metrica installation implemented

--- top-app/pages/_app.tsx
+++ top-app/pages/_app.tsx
@@ -2,18 +2,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { JSX } from "react";
+import ym from "react-yandex-metrika";
+import { YMInitializer } from "react-yandex-metrika";
 
 export default function App({
   Component,
   pageProps,
   router,
 }: AppProps): JSX.Element {
+  router.events.on("routeChangeComplete", (url: string) => {
+    if (typeof window !== "undefined") {
+      ym("hit", url);
+    }
+  });
+
   return (
     <>
       <Head>
         <title>Ratings-app</title>
         <link key={1} rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -29,6 +38,11 @@ export default function App({
         />
         <meta property="ag:locale" content="ru_RU" />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
